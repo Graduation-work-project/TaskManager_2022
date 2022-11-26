b29332e96317af6597e0f332fcb015647c38164b
riwltnchgo0625 <63505725+riwltnchgo0625@users.noreply.github.com>
2022-11-27 00:11:55 +0900

    알람버튼 수정, toast메시지 추가

--- app/src/main/java/com/example/myapplication/AlarmUi/AlarmLandingPageActivity.java
+++ app/src/main/java/com/example/myapplication/AlarmUi/AlarmLandingPageActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +34,7 @@ public final class AlarmLandingPageActivity extends AppCompatActivity {
         button1  = (Button) findViewById(R.id.load_main_activity_btn);
         button2 = (Button) findViewById(R.id.dismiss_btn);
 
+        //alarm main page 이동
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,14 +42,24 @@ public final class AlarmLandingPageActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
 
+                Toast myToast = Toast.makeText(getApplicationContext(),"알람 설정 페이지로 이동합니다.", Toast.LENGTH_SHORT);
+                myToast.show();
+
             }
         });
 
+        //디바이스 배경화면으로 이동
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
-                System.exit(0);
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+                Toast myToast = Toast.makeText(getApplicationContext(),"알람이 해제되었습니다.", Toast.LENGTH_SHORT);
+                myToast.show();
+
             }
         });
 
